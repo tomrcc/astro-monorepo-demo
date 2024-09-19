c3712a3d691055a531f0da7aec6fc50465bb8f40
tomrcc <tom@cloudcannon.com>
2024-09-19 12:26:26 +1200

    Refactor to monorepo

---
+++ dist/chunks/blog_Vi2xNJZf.mjs
@@ -0,0 +1,9 @@
+async function getMod() {
+						return import('./blog_Dz9KBUQx.mjs');
+					}
+					const collectedLinks = [];
+					const collectedStyles = [];
+					const collectedScripts = [];
+					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
+
+export { defaultMod as default };
